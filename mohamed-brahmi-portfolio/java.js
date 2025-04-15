83db8a23fa0925daa26b5bed56e5f95bd0f539db
nouhaila1995 <rosawhitefairy@gmail.com>
2025-04-15 17:52:43 +0100

    Add files via upload
    first try

---
+++ mohamed-brahmi-portfolio/java.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contactBtn = document.querySelector(".btn");
+  contactBtn.addEventListener("click", function () {
+    alert("You will be redirected to the contact section.");
+  });
+});
